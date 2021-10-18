952ad82988d8b060f15ed20fb511262c7a57367a
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-10-18 08:37:52 +0200

    Add files via upload

---
+++ ZadaciGabrielMatosevic20211018/Sport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciGabrielMatosevic20211018
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(string naziv)
+        {
+            Naziv = naziv;
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+            BrojIgraca = brojIgraca;
+        }
+    }
+}
